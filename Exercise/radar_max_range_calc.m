8e10214017c2c0951af833cf25f4746c5d309c39
Endymion Cheung <endymion.cheung@gmail.com>
2020-03-03 22:22:01 -0500

    MATLAB Exercise: Maximum Range Calculation
    Implemented the range equation to compute the radar range

---
+++ Exercise/radar_max_range_calc.m
@@ -0,0 +1,28 @@
+% Maximum Range Calculation
+
+%Operating frequency (Hz)
+fc = 77.0e9;
+
+%Transmitted power (W)
+Pt = 3e-3;
+
+%Antenna Gain (linear)
+G =  10000;
+
+%Minimum Detectable Power
+Ps = 1e-10;
+
+%RCS of a car
+RCS = 100;
+
+%Speed of light
+c = 3*10^8;
+
+%TODO: Calculate the wavelength
+lambda = c / fc;
+
+%TODO : Measure the Maximum Range a Radar can see.
+range = ((Ps*G^2*lambda^2*RCS)/(Pt*(4*pi())^3))^(1/4);
+
+% Print the range
+disp(range);
